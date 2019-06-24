a3b39b8bb3c2a8b9ded84f2a0802a9c7e7a37003
damutou95 <xzcjiangxi@163.com>
2019-06-24 23:24:55 +0800

    6月24号新增一题

--- test.py
+++ test.py
@@ -1,37 +1,16 @@
-def compareSort(a,b):
-    if a+b > b+a:
-        return True
-
-def partition(origin,p,q):
-    tag = origin[p]
-    l = p + 1
-    r = q
-    while True:
-        while l <= r and compareSort(tag,origin[l]):
-            l += 1
-        while l <= r and compareSort(origin[r],tag):
-            r -= 1
-        if r < l:
-            break
-        origin[l], origin[r] = origin[r], origin[l]
-    origin[r], origin[p] = origin[p], origin[r]
-    return r
-
-def fastSortHelper(origin,p,q):
-    if p<q:
-        mid = partition(origin,p,q)
-        fastSortHelper(origin,p,mid-1)
-        fastSortHelper(origin,mid+1,q)
-
-def fastSort(origin):
-    p = 0
-    q = len(origin) - 1
-    fastSortHelper(origin,p,q)
-
-
-if __name__ == '__main__':
-    a = [54,26,93,17,77,31,44,55,20]
-    fastSort(a)
-    print(a)
-
-
+class Solution:
+    def PrintMinNumber(self, numbers):
+        # write code here
+        numbers = [str(i) for i in numbers]
+        n = len(numbers)
+        while n > 0:
+            maxi = 0
+            for i in range(n):
+                if self.compareSort(numbers[i],numbers[maxi]):
+                    maxi = i
+            numbers[maxi],numbers[n-1] = numbers[n-1],numbers[maxi]
+            n -= 1
+        return ''.join(numbers)
+    def compareSort(self,a,b):
+        if a+b > b+a:
+            return True
